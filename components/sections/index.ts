ea720c62185ecb1f147706a954265b58c2aac4ab
akhiltj-ux <akhil.thanaly.jayaprakash@gmail.com>
2026-06-08 17:37:03 +0100

    Refractored file structure

--- components/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AboutHero } from './AboutHero';
-export { default as Footer } from './Footer';
-export { default as Header } from './Header';
-export { default as Hero } from './Hero';
-export { default as WorkGrid } from './WorkGrid';
